e8c4b00adc13346c35b8eb64e829ce8f9e3421ad
HAMMPC <bukowo17@gmail.com>
2017-06-03 14:05:34 +0200

    Initial commit

---
+++ app/src/main/java/nl/avans/mm/atps_assistent/MainActivity.java
@@ -0,0 +1,21 @@
+package nl.avans.mm.atps_assistent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		WebView webView = (WebView)findViewById(R.id.webView);
+//you can load an html code
+		//webView.loadData("yourCode Html to load on the webView " , "text/html" , "utf-8");
+// you can load an URL
+		webView.loadUrl("http://188.205.24.245/pctwa/login.aspx");
+
+	}
+}
