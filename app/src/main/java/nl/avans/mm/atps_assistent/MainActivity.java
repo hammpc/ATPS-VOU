6652f23c492d1604f9725d0ff35788dd360b0d87
HAMMPC <bukowo17@gmail.com>
2017-06-03 14:57:59 +0200

    Test 1

--- app/src/main/java/nl/avans/mm/atps_assistent/MainActivity.java
+++ app/src/main/java/nl/avans/mm/atps_assistent/MainActivity.java
@@ -2,9 +2,13 @@ package nl.avans.mm.atps_assistent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
+	
+	public final String GlobalUrl = "http://188.205.24.245/pctwa/login.aspx";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +16,15 @@ public class MainActivity extends AppCompatActivity {
 		setContentView(R.layout.activity_main);
 		
 		WebView webView = (WebView)findViewById(R.id.webView);
-//you can load an html code
-		//webView.loadData("yourCode Html to load on the webView " , "text/html" , "utf-8");
-// you can load an URL
-		webView.loadUrl("http://188.205.24.245/pctwa/login.aspx");
-
+		loadWebViewLoad(webView);
+	}
+	
+	private void loadWebViewLoad(WebView webview) {
+		webview.getSettings().setJavaScriptEnabled(true);
+		webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+		webview.getSettings().setSupportMultipleWindows(true);
+		webview.setWebViewClient(new WebViewClient());
+		webview.setWebChromeClient(new WebChromeClient());
+		webview.loadUrl(GlobalUrl);
 	}
 }
